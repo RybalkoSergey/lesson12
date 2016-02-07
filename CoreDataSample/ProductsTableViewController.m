68fd3e43c47c5c8f6cfdfdb7793c7852fc57d2fc
Sergey Rybalko <Rybalko.Sergey.s@gmail.com>
2016-02-07 19:58:38 +0200

    no message

--- CoreDataSample/ProductsTableViewController.m
+++ CoreDataSample/ProductsTableViewController.m
@@ -15,7 +15,7 @@
 #import "ProductDetailViewController.h"
 
 
-@interface ProductsTableViewController () <UITableViewDelegate, UIActionSheetDelegate>
+@interface ProductsTableViewController () <UITableViewDelegate, UIActionSheetDelegate, NSFetchedResultsControllerDelegate>
 
 @property (strong, nonatomic) NSArray *items;
 @property (strong, nonatomic) NSArray *soldItems;
@@ -24,6 +24,7 @@
 @property (nonatomic, strong) CDProduct *selectedProduct;
 @property (strong, nonatomic) UIActionSheet *actionSheet;
 
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
 
 @end
 
@@ -43,13 +44,13 @@
     
     [self fetchProducts];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addNewProduct:)];
-    [self refreshData];
+    //[self refreshData];
     
     self.actionSheet = [[UIActionSheet alloc] initWithTitle:@"What do you want to do with current product?"
-                                                             delegate:self
-                                                    cancelButtonTitle:@"Cancel"
-                                               destructiveButtonTitle:@"Delete"
-                                                    otherButtonTitles:@"Edit", @"Buy", nil];
+                                                   delegate:self
+                                          cancelButtonTitle:@"Cancel"
+                                     destructiveButtonTitle:@"Delete"
+                                          otherButtonTitles:@"Edit", @"Buy", nil];
 }
 
 #pragma mark - Private methods
@@ -89,7 +90,7 @@
 -(void) createProductWithName:(NSString *)name {
     NSManagedObjectContext *context = [CoreDataManager sharedInstance].managedObjectContext;
     CDProduct *product = [NSEntityDescription insertNewObjectForEntityForName:[[CDProduct class] description]
-                                                     inManagedObjectContext:context];
+                                                       inManagedObjectContext:context];
     product.name = name;
     [self.basket addProductsObject:product];
     [[CoreDataManager sharedInstance] saveContext];
@@ -107,6 +108,7 @@
     [self refreshData];
 }
 
+#pragma mark - NSFetch result controller
 
 -(NSArray *) fetchProducts {
     NSManagedObjectContext *context = [CoreDataManager sharedInstance].managedObjectContext;
@@ -115,8 +117,98 @@
     request.predicate = predicate;
     NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES];
     request.sortDescriptors = @[sortDescriptor];
+    
+    self.fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:request managedObjectContext:context sectionNameKeyPath:@"complete" cacheName:nil];
+    
+    // Configure Fetched Results Controller
+    [self.fetchedResultsController setDelegate:self];
+    
+    // Perform Fetch
+    NSError *error = nil;
+    [self.fetchedResultsController performFetch:&error];
+    
+    if (error) {
+        NSLog(@"Unable to perform fetch.");
+        NSLog(@"%@, %@", error, error.localizedDescription);
+    }
+    
     return [context executeFetchRequest:request error:nil];
 }
+
+- (void)controllerWillChangeContent:(NSFetchedResultsController *)controller {
+    [self.tableView beginUpdates];
+}
+
+- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
+    [self.tableView endUpdates];
+}
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type newIndexPath:(NSIndexPath *)newIndexPath {
+    switch (type) {
+        case NSFetchedResultsChangeInsert: {
+            [self.tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+        }
+        case NSFetchedResultsChangeDelete: {
+            [self.tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+        }
+        case NSFetchedResultsChangeUpdate: {
+            [self configureCell:(UITableViewCell *)[self.tableView cellForRowAtIndexPath:indexPath] atIndexPath:indexPath];
+            break;
+        }
+        case NSFetchedResultsChangeMove: {
+            [self.tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+            [self.tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+        }
+    }
+}
+
+- (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath {
+    
+    CDProduct *product;
+    if (indexPath.section == 0) {
+        NSArray *notSoldItems = [self getNotSoldProducts:[self.fetchedResultsController fetchedObjects]];
+        product = notSoldItems[indexPath.row];
+    } else {
+        NSArray *soldItems = [self getSoldProducts:[self.fetchedResultsController fetchedObjects]];
+        product = soldItems[indexPath.row];
+    }
+    
+    //    product = [self.fetchedResultsController objectAtIndexPath:indexPath];
+    
+    //    NSLog(@"%@", [product complete]);
+    cell.textLabel.text = product.name;
+    if ([product.complete boolValue]) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    } else {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    
+}
+
+-(NSArray *) getNotSoldProducts: (NSArray *)items {
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    for (CDProduct *item in items) {
+        if ((int)item.complete == 0 || item.complete == nil) {
+            [result addObject:item];
+        }
+    }
+    return result;
+}
+
+-(NSArray *) getSoldProducts: (NSArray *)items {
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    for (CDProduct *item in items) {
+        NSLog(@"%@", item.complete);
+        if ([item.complete isEqual:[NSNumber numberWithInt:1]]) {
+            [result addObject:item];
+        }
+    }
+    return result;
+}
+
 
 -(NSArray *) fetchSoldProducts {
     NSManagedObjectContext *context = [CoreDataManager sharedInstance].managedObjectContext;
@@ -142,77 +234,53 @@
 #pragma mark - Table view data source
 
 -(void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    
-    if (indexPath.section == 0) {
-        self.selectedProduct = self.soldItems[indexPath.row];
-    } else {
-        self.selectedProduct = self.notSoldItems[indexPath.row];
-    }
-    
-    //    if ([product.complete boolValue]) {
-    //        product.complete = @NO;
-    //    } else {
-    //        product.complete = @YES;
-    //    }
-    //    [self.tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    self.selectedProduct = self.items[indexPath.row];
     
     [self.actionSheet showInView:self.view];
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 2;
+    return [[self.fetchedResultsController sections] count];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    if (section == 0) {
-        
-        return [self.soldItems count];
-        
-    } else {
-        
-        return [self.notSoldItems count];
-    }
+    NSArray *sections = [self.fetchedResultsController sections];
+    id<NSFetchedResultsSectionInfo> sectionInfo = [sections objectAtIndex:section];
+    
+    return [sectionInfo numberOfObjects];
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
 {
     if (section == 0) {
-        return @"SOLD";
-    } else {
         return @"AVAILABLE";
+    } else {
+        return @"SOLD";
     }
+    //    if ([[self.fetchedResultsController sections] count] > 0) {
+    //        id <NSFetchedResultsSectionInfo> sectionInfo = [[self.fetchedResultsController sections] objectAtIndex:section];
+    //        return [sectionInfo name];
+    //    } else
+    //        return nil;
 }
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CellIdentifier" forIndexPath:indexPath];
     
-    CDProduct *product;
-    if (indexPath.section == 0) {
-        product = self.soldItems[indexPath.row];
-    } else {
-        product = self.notSoldItems[indexPath.row];
-    }
+    [self configureCell:cell atIndexPath:indexPath];
     
-    
-//    NSLog(@"%@", [product complete]);
-    cell.textLabel.text = product.name;
-    if ([product.complete boolValue]) {
-        cell.accessoryType = UITableViewCellAccessoryCheckmark;
-    } else {
-        cell.accessoryType = UITableViewCellAccessoryNone;
-    }
     return cell;
 }
 
 
 /*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
+ // Override to support conditional editing of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the specified item to be editable.
+ return YES;
+ }
+ */
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
@@ -225,7 +293,7 @@
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
     } else if (editingStyle == UITableViewCellEditingStyleInsert) {
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+    }
 }
 
 -(void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex{
@@ -252,45 +320,45 @@
         [controller addAction:action];
         [self presentViewController:controller animated:YES completion:NULL];
     }
-//    NSLog(@"Index = %ld - Title = %@", (long)buttonIndex, [actionSheet buttonTitleAtIndex:buttonIndex]);
+    //    NSLog(@"Index = %ld - Title = %@", (long)buttonIndex, [actionSheet buttonTitleAtIndex:buttonIndex]);
 }
 
 -(void) markProductAsSoldWithPrice:(NSDecimalNumber *)price {
     self.selectedProduct.price = price;
     self.selectedProduct.complete = @YES;
-
+    
     NSError *saveError = nil;
     if (![self.selectedProduct.managedObjectContext save:&saveError]) {
         NSLog(@"Unable to save managed object context.");
         NSLog(@"%@, %@", saveError, saveError.localizedDescription);
     }
     NSIndexPath *indexPath = [NSIndexPath indexPathForRow:[self.items indexOfObject:self.selectedProduct] inSection:0];
-//    [self.tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    //    [self.tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
     [self refreshData];
 }
 
 /*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
+ // Override to support rearranging the table view.
+ - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+ }
+ */
 
 /*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
+ // Override to support conditional rearranging of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the item to be re-orderable.
+ return YES;
+ }
+ */
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
